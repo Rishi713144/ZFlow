693aedc8be7b468b362377491fe61d38be6d7f1c
Rishi <116449371+Rishi713144@users.noreply.github.com>
2026-02-02 15:54:40 +0530

    fix: update backend URL and server port for consistency; add database check script

---
+++ primary-backend/check_db.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const pg_1 = require("pg");
+const adapter_pg_1 = require("@prisma/adapter-pg");
+const connectionString = "postgresql://postgres:password@localhost:5432/zapier";
+const pool = new pg_1.Pool({ connectionString });
+const adapter = new adapter_pg_1.PrismaPg(pool);
+const prisma = new client_1.PrismaClient({ adapter });
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const userCount = yield prisma.user.count();
+            console.log(`User count: ${userCount}`);
+            const actionCount = yield prisma.availableAction.count();
+            console.log(`AvailableAction count: ${actionCount}`);
+        }
+        catch (e) {
+            console.error(e);
+        }
+        finally {
+            yield prisma.$disconnect();
+        }
+    });
+}
+main();
